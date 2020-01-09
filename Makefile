b638017fc4044f97c0777cb6e42917b676eb555b
ring0hacking <r1ng0hacking@gmail.com>
2020-01-09 10:16:49 +0200

    fs type info print

--- Makefile
+++ Makefile
@@ -1,6 +1,6 @@
 obj-m := fs_test.o
 
 all:
-	make -C /usr/src/linux-headers-`uname -r` M=`pwd` modules
+	make -C /usr/src/linux-headers-`uname -r` M=`pwd` modules && insmod fs_test.ko && rmmod fs_test
 clean:
 	make -C /usr/src/linux-headers-`uname -r` M=`pwd` clean
--- fs_test.c
+++ fs_test.c
@@ -1,10 +1,55 @@
 #include <linux/kernel.h>
 #include <linux/init.h>
 #include <linux/module.h>
+#include <linux/kallsyms.h>
+#include <linux/rwlock.h>
+#include <linux/rwlock_types.h>
+#include <linux/mm.h>
+#include <linux/highmem.h>
+#include <linux/fs.h>
+
+static struct file_system_type *file_systems;
+static rwlock_t *file_systems_lock;
+
+static int get_symbols(void)
+{
+	file_systems = *(struct file_system_type **)kallsyms_lookup_name("file_systems");
+	if(file_systems == NULL){
+		printk("Not found file_systems symbol");
+		return -1;
+	}
+	
+	file_systems_lock = (rwlock_t *)kallsyms_lookup_name("file_systems_lock");
+	if(file_systems == NULL){
+		printk("Not found file_systems_lock symbol");
+		return -1;
+	}
+	return 0;
+}
+
+static void print_fs_type(void)
+{
+	struct file_system_type *tmp;
+	
+	read_lock(file_systems_lock);
+
+	tmp = file_systems;
+	while(tmp){
+		printk("fs type name:%s\n",tmp->name);
+		tmp = tmp->next;
+	}
+	
+	read_unlock(file_systems_lock);
+}
 
 static int __init fs_test_init(void)
 {
-	printk("fs_test_init func\n");
+
+	if(get_symbols() != 0){
+		return -1;
+	}
+	
+	print_fs_type();
 
 	return 0;
 }
